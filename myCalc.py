48701b7404d85480e54cc62772b776319092eea4
SonYeJin <sig009948@naver.com>
2022-10-25 15:40:23 +0900

    First Commit 프로젝트를 지금부터 깃허브에서 관리함.

---
+++ myCalc.py
@@ -0,0 +1,56 @@
+# 함수
+def add_func(n1, n2):
+    return n1+n2
+
+def sub_func(n1, n2):
+    return n1-n2
+
+def mul_func(n1, n2):
+    return n1*n2
+
+def div_func(n1, n2):
+    return n1/n2
+
+<<<<<<< HEAD
+def ret_func(n1):
+    return n1
+=======
+def rem_func(n1, n2) :
+    return n1//n2
+
+def pow_func(n1, n2) :
+    return n1**n2
+
+>>>>>>> newCalc
+
+# 전역
+num1, num2, result = 100, 200, 0
+
+# 메인
+result = add_func(num1, num2)
+print(num1, "+", num2, "=", result)
+
+result = sub_func(num1, num2)
+print(num1, "-", num2, "=", result)
+
+result = mul_func(num1, num2)
+print(num1, "*", num2, "=", result)
+
+result = div_func(num1, num2)
+print(num1, "*", num2, "=", result)
+
+<<<<<<< HEAD
+result = ret_func(num1)
+print(num1, "=", result)
+=======
+res = rem_func(num1, num2)
+print(num1, "//", num2, "=", res)
+
+res = pow_func(num1, num2)
+print(num1, "**", num2, "=", res)
+
+>>>>>>> newCalc
+
+
+
+
